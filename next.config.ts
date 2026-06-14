f57d4660a1286eb68d3ae902068d3d2368e0f0ca
Hrithik-41 <littlehrithik9594@gmail.com>
2026-06-14 08:49:23 +0530

    Fix GitHub Pages paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/my-garage",
+  assetPrefix: "/my-garage/",
 };
 
 export default nextConfig;
